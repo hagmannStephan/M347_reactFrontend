462a22e19be1e0f8bdd3bd7f9a4900ed7a270ac8
esilva <esilva@ethz.ch>
2024-06-28 09:54:22 +0200

    Refactored trips path

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -23,47 +23,60 @@ function TripList({ dispatch }) {
       <section>
         <h4 className="h4">Business Trips - Planned 2021</h4>
         <div className="row">
-          <ul>
-            {trips.map(item => (
-              <li key={item.id}>{item.title}</li>
-            ))}
-          </ul>
+          <table className="tripsTable">
+            <thead className="tripsTablehead">
+              <tr>
+                <th>Title</th>
+                <th>Employees</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {trips.map(item => (
+                <tr className="tripsTablebody" key={item.idBusinessTrip}>
+                  <td>{item.title}</td>
+                  <td>{item.employees}</td>
+                  <td>{item.description}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </section>
     </div>
   );
 }
 
-function Trip({ dispatch, trip }) {
-  let { id, title, description, startTrip, endTrip } = trip;
+// function Trip({ dispatch, trip }) {
+//   let { id, title, description, startTrip, endTrip } = trip;
 
-  return (
-    <div className="col-sm-6 col-md-4 col-lg-3">
-      <figure className="card card-product">
-        <div className="img-wrap">
-          <img src={"images/items/" + id + ".jpg"} alt="name " />
-        </div>
-        <figcaption className="info-wrap">
-          <a href="#. . . " className="title">
-            {title}
-          </a>
-          <div className="price-wrap">
-            <span className="price-new">{startTrip}</span>
-          </div>
-          <p className="card-text">{description}</p>
-          <div className="info-wrap row">
-            <button
-              type="button"
-              className="btn btn-link btn-outline"
-              onClick={() => dispatch({ type: "add", ...trip })}
-            >
-              <i /*className="fa fa-luggage-cart"*/ /> Add to Triplist
-            </button>
-          </div>
-        </figcaption>
-      </figure>
-    </div>
-  );
-}
+//   return (
+//     <div className="col-sm-6 col-md-4 col-lg-3">
+//       <figure className="card card-product">
+//         <div className="img-wrap">
+//           <img src={"images/items/" + id + ".jpg"} alt="name " />
+//         </div>
+//         <figcaption className="info-wrap">
+//           <a href="#. . . " className="title">
+//             {title}
+//           </a>
+//           <div className="price-wrap">
+//             <span className="price-new">{startTrip}</span>
+//           </div>
+//           <p className="card-text">{description}</p>
+//           <div className="info-wrap row">
+//             <button
+//               type="button"
+//               className="btn btn-link btn-outline"
+//               onClick={() => dispatch({ type: "add", ...trip })}
+//             >
+//               <i /*className="fa fa-luggage-cart"*/ /> Add to Triplist
+//             </button>
+//           </div>
+//         </figcaption>
+//       </figure>
+//     </div>
+//   );
+// }
 
 export default TripList;
