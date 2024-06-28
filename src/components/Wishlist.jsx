33fd76c1cfe1fd0bea7b6f01afa2b29a412dc8fa
Gian Jaya <70436710+GianJaya@users.noreply.github.com>
2024-06-28 16:20:19 +0200

    try to fix test pipeline errors

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./Wishlist.css";
 
 export default function Wishlist(props) {
