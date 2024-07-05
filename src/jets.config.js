409a02d0319c8669218e5b5538568c1242dd3843
Gian Jaya <70436710+GianJaya@users.noreply.github.com>
2024-07-05 10:40:07 +0200

    test

--- src/jets.config.js
+++ src/jets.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
     transform: {
       "^.+\\.[t|j]sx?$": "babel-jest",
       "^.+\\.css$": "jest-transform-css"
@@ -7,7 +8,7 @@ module.exports = {
       "\\.(css|less|sass|scss)$": "identity-obj-proxy"
     },
     transformIgnorePatterns: [
-      "/node_modules/(?!@testing-library)"
+      "/node_modules/(?!@babel|@testing-library)"
     ]
   };
   
