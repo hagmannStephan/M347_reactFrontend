7d5941ff74d6edecc4259880181b35f9ed7baf7d
Gian Jaya <70436710+GianJaya@users.noreply.github.com>
2024-07-05 11:14:43 +0200

    test fix

--- jets.config.js
+++ jets.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-    setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
-    transform: {
-      "^.+\\.[t|j]sx?$": "babel-jest",
-      "^.+\\.css$": "jest-transform-css"
-    },
-    moduleNameMapper: {
-      "\\.(css|less|sass|scss)$": "identity-obj-proxy"
-    },
-    transformIgnorePatterns: [
-      "/node_modules/(?!@babel|@testing-library)"
-    ]
-  };
-  
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+    "^.+\\.css$": "jest-transform-css"
+  },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy"
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!@babel|@testing-library)"
+  ]
+};
