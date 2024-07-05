c49c223a326b95702f4fdfb44c4a3407ddd1907e
esilva <esilva@ethz.ch>
2024-07-05 09:59:46 +0200

    Made last fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
-
-const activeStyle = {
-  color: "purple",
-};
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
